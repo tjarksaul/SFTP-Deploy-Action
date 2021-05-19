65be45397558a78d68e010c4152b23c51a591add
Tjark Saul <113299+tjarksaul@users.noreply.github.com>
2021-05-19 11:08:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'sftp start'
 # create a temporary file containing sftp commands
-printf "%s\n" "mkdir $6" >$TEMP_SFTP_FILE
-(cd $5; find * .* -type d -exec echo mkdir $6/{} \;) >>$TEMP_SFTP_FILE
+printf "%s\n" "-mkdir $6" >$TEMP_SFTP_FILE
+(cd $5; find * .* -type d -exec echo -mkdir $6/{} \;) >>$TEMP_SFTP_FILE
 printf "%s" "put -r $5 $6" >>$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
 sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
